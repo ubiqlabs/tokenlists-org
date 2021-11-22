8f0ccb4b05636a97fd72caf1bbf26766d40e6235
Jaimish Patel <jaimish.patel95@gmail.com>
2021-11-22 13:12:21 -0500

    1.0.0

---
+++ precache-manifest.d3e9076dff6684f6aa3ba18426d70a1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcd648b332825e2fa0f930996350267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75cebde4de0405fe8958",
+    "url": "/static/css/main.f1e601b4.chunk.css"
+  },
+  {
+    "revision": "6c697beb03a716414b75",
+    "url": "/static/js/2.c03b4fe3.chunk.js"
+  },
+  {
+    "revision": "24340b6599826509885a24d802f9ab0e",
+    "url": "/static/js/2.c03b4fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cebde4de0405fe8958",
+    "url": "/static/js/main.bbf6cb13.chunk.js"
+  },
+  {
+    "revision": "c7aea05a5b0c0aaced8b",
+    "url": "/static/js/runtime-main.714af41d.js"
+  },
+  {
+    "revision": "0c2988715025b42c8778c5854ff910f9",
+    "url": "/static/media/MatterSQ-Medium.0c298871.woff2"
+  },
+  {
+    "revision": "0d5acd5713a17574f15dfd7f56365743",
+    "url": "/static/media/MatterSQ-Regular.0d5acd57.woff2"
+  },
+  {
+    "revision": "a70cdfd60b0bc838aa99cc95d1506c45",
+    "url": "/static/media/MatterSQ-SemiBold.a70cdfd6.woff2"
+  },
+  {
+    "revision": "d2f9ef7a556e8f0ba92292736340a8a2",
+    "url": "/static/media/color.d2f9ef7a.png"
+  }
+]);
